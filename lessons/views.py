15ea871d698fa5dda74853c89075cad0af85be54
Abeer-teacher <holygreator@gmail.com>
2026-06-02 14:22:18 +0300

    fix internal server error

--- lessons/views.py
+++ lessons/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 
 def lesson(request, letter):
+    # توليد كل الحروف العربية
     letters = [chr(i) for i in range(0x0627, 0x064A + 1)]
 
+    # حماية من الخطأ
     if letter not in letters:
         return render(request, 'home.html')
 
     index = letters.index(letter)
 
+    # السابق والتالي
     prev_letter = letters[index - 1] if index > 0 else None
     next_letter = letters[index + 1] if index < len(letters) - 1 else None
 
+    # بيانات الدرس
     lesson = {
         "letter": letter,
         "examples": [],
