90e182ca6efed26053949fb729e1ad810d8d82a3
Abeer-teacher <holygreator@gmail.com>
2026-06-02 13:58:53 +0300

    fix home url

--- arabic_schools/urls.py
+++ arabic_schools/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from lessons import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lessons.urls')),
+
+    # 👇 ده أهم سطر
+    path('', views.lesson, {'letter': 'ب'}),
+
+    path('lesson/<str:letter>/', views.lesson),
 ]
